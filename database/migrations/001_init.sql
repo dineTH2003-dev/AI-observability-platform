76103c413b5938e34f0c35e131b999e180498c6d
Hashini Fernando <hashinifdo04@gmail.com>
2026-01-17 23:35:21 -0500

    Update DB Schema

--- database/migrations/001_init.sql
+++ database/migrations/001_init.sql
@@ -13,3 +13,32 @@ CREATE TABLE IF NOT EXISTS logs (
   payload JSONB,
   created_at TIMESTAMP WITH TIME ZONE DEFAULT now()
 );
+
+
+CREATE TABLE applications (
+    app_id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description TEXT,
+    version VARCHAR(50),
+    server_id INT NOT NULL,
+    status VARCHAR(20) DEFAULT 'ACTIVE',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_app_server
+        FOREIGN KEY (server_id)
+        REFERENCES servers(server_id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE servers (
+    server_id SERIAL PRIMARY KEY,
+    hostname TEXT NOT NULL,
+    ip_address TEXT NOT NULL,
+    username TEXT NOT NULL,
+    ssh_port INTEGER NOT NULL DEFAULT 22,
+    environment TEXT,
+    description TEXT,
+    agent_installed BOOLEAN NOT NULL DEFAULT FALSE,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
